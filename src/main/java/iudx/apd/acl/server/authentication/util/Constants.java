f5d5c5fd71bb25d49d21dbf142d3412177a5659e
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2025-01-11 21:38:50 +0530

    Refactor: user access handler

--- src/main/java/iudx/apd/acl/server/authentication/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/authentication/util/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
   public static final String INSERT_USER_TABLE = "insert into user_table(_id,email_id,first_name,last_name) " +
       " values ($1,$2,$3,$4)" +
       " on conflict (_id) do update SET (email_id,first_name,last_name) " +
-      "= (EXCLUDED.email_id,EXCLUDED.first_name,EXCLUDED.last_name);";
+      "= (EXCLUDED.email_id,EXCLUDED.first_name,EXCLUDED.last_name) RETURNING _id;";
   public static final String GET_USER = "select * from user_table where _id=$1::UUID;";
   public static final int JWT_LEEWAY_TIME = 30;
 }
