1bee9ee51244105053660770734132d173e28d24
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2025-01-13 14:47:58 +0530

    Refactor: test insert query

--- src/main/java/iudx/apd/acl/server/authentication/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/authentication/util/Constants.java
@@ -10,7 +10,8 @@ public class Constants {
   /*user's information is updated as soon as the user uses any provider or consumer respective API*/
   public static final String INSERT_USER_TABLE = "insert into user_table(_id,email_id,first_name,last_name) " +
       " values ($1,$2,$3,$4)" +
-      " on conflict (_id) do nothing;";
+      " on conflict (_id) do update SET (email_id,first_name,last_name) " +
+      "= (EXCLUDED.email_id,EXCLUDED.first_name,EXCLUDED.last_name)";
   public static final String GET_USER = "select * from user_table where _id=$1::UUID;";
   public static final int JWT_LEEWAY_TIME = 30;
 }
