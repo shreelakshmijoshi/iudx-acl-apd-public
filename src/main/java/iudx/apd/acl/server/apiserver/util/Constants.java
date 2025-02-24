e3a20dfd7570066a0326ea03b25f7a2f10c926e2
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2025-02-24 17:25:42 +0530

    Refactor: get token

--- src/main/java/iudx/apd/acl/server/apiserver/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/apiserver/util/Constants.java
@@ -14,6 +14,7 @@ public class Constants {
   public static final String X_CONTENT_TYPE_OPTIONS_NOSNIFF = "nosniff";
 
   public static final String HEADER_TOKEN = "token";
+  public static final String HEADER_BEARER_AUTHORIZATION = "Authorization";
   public static final String HEADER_TOKEN_BEARER = "Bearer";
   public static final String HEADER_HOST = "Host";
   public static final String HEADER_ACCEPT = "Accept";
